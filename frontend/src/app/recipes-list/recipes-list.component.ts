26dc6932b4d6d316bc54ab1ff8c374368c97c01a
Josh McHugh <joshua.mchugh01@gmail.com>
2024-01-23 23:09:00 -0500

    Added primeflex and primeicon libraries and added remaining website framework to start chapter examples

--- frontend/src/app/recipes-list/recipes-list.component.ts
+++ frontend/src/app/recipes-list/recipes-list.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { DataViewModule } from 'primeng/dataview';
+import { RatingModule } from 'primeng/rating';
 
 @Component({
   selector: 'app-recipes-list',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, DataViewModule, FormsModule, RatingModule],
   templateUrl: './recipes-list.component.html',
   styleUrl: './recipes-list.component.scss'
 })
