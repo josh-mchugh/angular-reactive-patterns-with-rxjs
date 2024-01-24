a5d374cfc6a793fe5c6af65e1217865b0818992e
Josh McHugh <joshua.mchugh01@gmail.com>
2024-01-24 14:35:43 -0500

    Got the dataview working and wired up the HttpClient Observable with a subscribe and unsubscribed in an imperative way

--- frontend/src/app/recipes-list/recipes-list.component.ts
+++ frontend/src/app/recipes-list/recipes-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { ButtonModule } from 'primeng/button';
 import { DataViewModule } from 'primeng/dataview';
 import { RatingModule } from 'primeng/rating';
@@ -13,14 +14,21 @@ import { RecipesService } from '../core/services/recipes.service';
   templateUrl: './recipes-list.component.html',
   styleUrl: './recipes-list.component.scss'
 })
-export class RecipesListComponent implements OnInit{
+export class RecipesListComponent implements OnInit, OnDestroy {
   recipes!: Recipe[];
+  subscription: Subscription;
 
-  constructor(private service: RecipesService) { }
-
-  ngOnInit(): void {
-    this.service.getRecipes().subscribe(result => {
+  constructor(private service: RecipesService) {
+    this.subscription = this.service.getRecipes().subscribe(result => {
       this.recipes = result;
     });
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
