2a5bb33a4980dd35add3c45d26a37c6ecee54ae0
Josh McHugh <joshua.mchugh01@gmail.com>
2024-01-25 19:53:41 -0500

    Added example of 'Replace Strategy' from chapter 5

--- frontend/src/app/recipes-list/recipes-list.component.ts
+++ frontend/src/app/recipes-list/recipes-list.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, from, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { ButtonModule } from 'primeng/button';
 import { DataViewModule } from 'primeng/dataview';
 import { RatingModule } from 'primeng/rating';
@@ -13,6 +14,7 @@ import { RecipesService } from '../core/services/recipes.service';
   standalone: true,
   imports: [
     ButtonModule,
+
     CommonModule,
     DataViewModule,
     FormsModule,
@@ -27,5 +29,25 @@ export class RecipesListComponent {
 
   constructor(private service: RecipesService) {
     this.recipes$ = this.service.getRecipes();
+
+    // Chapter 5 - The Replace Strategy
+    const stream$ = from(['5', '10', '6', 'Hello', '2']);
+    stream$.pipe(
+      map((value) => {
+        if(isNaN(value as any)) {
+          throw new Error('This is not a number');
+        }
+        return parseInt(value);
+      }),
+      catchError((error) => {
+        console.log('Caught Error', error);
+        return of('!!Replaced Error Value!!');
+      })
+    )
+      .subscribe({
+        next: (res) => console.log('Value Emitted: ', res),
+        error: (err) => console.log('Error Occurred: ', err),
+        complete: () => console.log('Stream Complete'),
+      });
   }
 }
